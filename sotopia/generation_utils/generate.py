c382f83608476eef40ff001465ae5da54749f1e2
Hao Zhu <prokilchu@gmail.com>
2023-03-31 18:18:54 -0400

    ✨ generate support gpt-3/3.5/4

--- sotopia/generation_utils/generate.py
+++ sotopia/generation_utils/generate.py
@@ -1,11 +1,26 @@
 import langchain
+from beartype import beartype
+from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
+from langchain.schema import HumanMessage
+from typing_extensions import Literal
+
+LLM_Name = Literal["gpt-3.5-turbo", "text-davinci-003", "gpt-4"]
 
 
-def generate_scenario() -> str:
+@beartype
+def generate_scenario(model_name: LLM_Name) -> str:
     """
     Using langchain to generate a scenario
     """
-    llm = OpenAI()  # type: ignore[call-arg]
     text = "Please generate a plausible scenario for a conversation between a man and a woman:"
-    return llm(text)
+    match model_name:
+        case "gpt-3.5-turbo" | "gpt-4":
+            chat_llm = ChatOpenAI(model_name=model_name)  # type: ignore[call-arg]
+            message = HumanMessage(content=text)
+            return chat_llm([message]).content
+        case "text-davinci-003":
+            llm = OpenAI(model_name="text-davinci-003")  # type: ignore[call-arg]
+            return llm(text)
+        case _:
+            raise ValueError(f"Invalid model name: {model_name}")
--- tests/generation_utils/test_generation.py
+++ tests/generation_utils/test_generation.py
@@ -5,6 +5,6 @@ def test_generate_scenario() -> None:
     """
     Test that the scenario generator works
     """
-    scenario = generate_scenario()
+    scenario = generate_scenario("gpt-3.5-turbo")
     assert isinstance(scenario, str)
     assert len(scenario) > 0
