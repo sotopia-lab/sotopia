7ae50cd4fa9136f3abbc21df08f59a5ccaa0c22f
Hao Zhu <prokilchu@gmail.com>
2023-08-17 17:45:16 -0700

    Pin Pydantic==1.10 (#100)
    * ✨ make generating env profile configurable
    
    * 📌 langchain==0.263
    
    * 📌 change pin to pydantic==1.10
    
    * 🎨 pre commit
    
    * 📌 pydantic==1.10.12

--- stubs/peft/__init__.pyi
+++ stubs/peft/__init__.pyi
@@ -1,9 +1,16 @@
-from typing import Any
+from typing import Any, TypeVar
 
 import torch
-from transformers.utils.hub import PushToHubMixin
 
-class PeftModel(PushToHubMixin, torch.nn.Module): ...  # type: ignore[misc]
+Model = TypeVar("Model", bound=PushToHubMixin)
+
+class PushToHubMixin:
+    @classmethod
+    def from_pretrained(
+        cls: type[Model], *args: Any, **kwargs: Any
+    ) -> Model: ...
+
+class PeftModel(PushToHubMixin, torch.nn.Module): ...
 
 def set_peft_model_state_dict(
     model: PeftModel, peft_model_state_dict: dict[str, Any]
