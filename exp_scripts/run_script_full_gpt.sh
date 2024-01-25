251a7734ab017adf351c1a7626e4b417c445acf8
Zhe Su <360307598@qq.com>
2024-01-25 12:17:50 -0500

    fexperiment scripts and small bug fix

---
+++ exp_scripts/run_script_full_gpt.sh
@@ -0,0 +1,14 @@
+cd ../sotopia
+python examples/generate_script.py \
+ --gin_file sotopia_conf/generation_utils_conf/generate.gin \
+ --gin_file sotopia_conf/server_conf/server.gin \
+ --gin_file sotopia_conf/run_async_server_in_batch_script.gin \
+ '--gin.ENV_IDS=[]' \
+ '--gin.SCRIPT_MODEL="gpt-3.5-turbo"' \
+ '--gin.AGENT1_MODEL="gpt-3.5-turbo"' \
+ '--gin.AGENT2_MODEL="gpt-3.5-turbo"' \
+ '--gin.BATCH_SIZE=1' \
+ '--gin.TAG="script_full_gpt3.5_gpt3.5_rewrite"' \
+ '--gin.PUSH_TO_DB=True' \
+ '--gin.VERBOSE=False' \
+ '--gin.LITE=False' \
