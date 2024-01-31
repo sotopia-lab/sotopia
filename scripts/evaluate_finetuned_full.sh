5f342a96d1991f85fc6bdd3fb566c77e0dd43afb
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-01-31 14:07:44 -0500

    update scripts

--- scripts/evaluate_finetuned_full.sh
+++ scripts/evaluate_finetuned_full.sh
@@ -4,9 +4,9 @@ python examples/experiment_eval.py \
  --gin_file sotopia_conf/run_async_server_in_batch.gin \
  '--gin.ENV_IDS=[]' \
  '--gin.SCRIPT_MODEL="gpt-3.5-turbo-finetuned"' \
- '--gin.BATCH_SIZE=5' \
- '--gin.TAG="finetuned_eval_full"' \
- '--gin.TAG_TO_CHECK_EXISTING_EPISODES="finetuned_eval_full"' \
+ '--gin.BATCH_SIZE=1' \
+ '--gin.TAG="finetuned_eval_full_sotopia_normal"' \
+ '--gin.TAG_TO_CHECK_EXISTING_EPISODES="finetuned_eval_full_sotopia_normal"' \
  '--gin.PUSH_TO_DB=True' \
  '--gin.VERBOSE=False' \
  '--gin.LITE=True' \
