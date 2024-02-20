26eba2874c02de352a52706582397005f667841d
XuhuiZhou <zhouxuhui2018@gmail.com>
2024-02-20 15:34:21 -0500

    add csv& json

--- sotopia/database/__init__.py
+++ sotopia/database/__init__.py
@@ -7,6 +7,7 @@ from .persistent_profile import (
     RelationshipProfile,
     RelationshipType,
 )
+from .utils import episodes_to_csv, episodes_to_json, get_rewards_from_episode
 from .session_transaction import MessageTransaction, SessionTransaction
 from .waiting_room import MatchingInWaitingRoom
 
@@ -23,4 +24,7 @@ __all__ = [
     "SessionTransaction",
     "MessageTransaction",
     "MatchingInWaitingRoom",
+    "episodes_to_csv",
+    "episodes_to_json",
+    "get_rewards_from_episodes"
 ]
