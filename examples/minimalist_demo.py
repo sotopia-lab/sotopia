ce1eebd4e7f58a18e98bf725608dd87e24f6e84c
Hao <prokilchu@gmail.com>
2024-04-04 14:46:06 -0400

    Update server configuration in minimalist_demo.py

--- examples/minimalist_demo.py
+++ examples/minimalist_demo.py
@@ -4,13 +4,20 @@
 # 1.1. Import the `run_async_server` function: In sotopia, we use Python Async
 #     API to optimize the throughput.
 import asyncio
-from sotopia.server import run_async_server
+
 # 1.2. Import the `UniformSampler` class: In sotopia, we use samplers to sample
 #     the social tasks.
 from sotopia.samplers import UniformSampler
+from sotopia.server import run_async_server
 
 # 2. Run the server
-asyncio.run(run_async_server(
-    model_dict={"env": "gpt-4", "agent1": "gpt-3.5-turbo", "agent2": "gpt-3.5-turbo"},
-    sampler=UniformSampler()
-))
+asyncio.run(
+    run_async_server(
+        model_dict={
+            "env": "gpt-4",
+            "agent1": "gpt-3.5-turbo",
+            "agent2": "gpt-3.5-turbo",
+        },
+        sampler=UniformSampler(),
+    )
+)
