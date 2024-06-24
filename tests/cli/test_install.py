bedb16cedbed5fa0a5d50039059c982ef3edb3d1
Hao <prokilchu@gmail.com>
2024-06-23 22:51:23 -0400

    add use docker test

--- tests/cli/test_install.py
+++ tests/cli/test_install.py
@@ -1,6 +1,8 @@
+import platform
 from typer.testing import CliRunner
 
 from sotopia.cli import app
+import subprocess
 
 runner = CliRunner()
 
@@ -9,5 +11,16 @@ def test_install() -> None:
     result = runner.invoke(
         app,
         ["install", "--no-use-docker", "--load-database", "--overwrite-existing-data"],
+    )
+    assert result.exit_code == 0
+
+    if platform.system() == "Darwin":
+        subprocess.call("redis-cli shutdown")
+    elif platform.system() == "Linux":
+        subprocess.call("./redis-stack-server-7.2.0-v10/bin/redis-cli shutdown")
+
+    result = runner.invoke(
+        app,
+        ["install", "--use-docker", "--load-database", "--overwrite-existing-data"],
     )
     assert result.exit_code == 0
