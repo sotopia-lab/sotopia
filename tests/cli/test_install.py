a11bc164b975d4b225281415e31abec74348dccf
Hao <prokilchu@gmail.com>
2024-06-24 12:59:39 -0400

    avoid permission issue on mac

--- tests/cli/test_install.py
+++ tests/cli/test_install.py
@@ -9,28 +9,41 @@ runner = CliRunner()
 
 
 def test_install() -> None:
-    result = runner.invoke(
-        app,
-        ["install", "--no-use-docker", "--load-database", "--overwrite-existing-data"],
-    )
-    assert result.exit_code == 0
-    time.sleep(1)
-
     if platform.system() == "Darwin":
+        result = runner.invoke(
+            app,
+            ["install", "--no-use-docker", "--overwrite-existing-data"],
+        )
+        assert result.exit_code == 0
+        time.sleep(1)
         subprocess.run("redis-cli shutdown", shell=True, check=True)
     elif platform.system() == "Linux":
+        result = runner.invoke(
+            app,
+            [
+                "install",
+                "--no-use-docker",
+                "--load-database",
+                "--overwrite-existing-data",
+            ],
+        )
+        assert result.exit_code == 0
+        time.sleep(1)
         subprocess.run(
             "./redis-stack-server-7.2.0-v10/bin/redis-cli shutdown",
             shell=True,
             check=True,
         )
 
-    result = runner.invoke(app, ["install"], input="No\nNo\n")
-    assert result.exit_code == 0
-    time.sleep(1)
     if platform.system() == "Darwin":
+        result = runner.invoke(app, ["install"], input="Yes\nNo\n\n")
+        assert result.exit_code == 0
+        time.sleep(1)
         subprocess.run("redis-cli shutdown", shell=True, check=True)
     elif platform.system() == "Linux":
+        result = runner.invoke(app, ["install"], input="No\nNo\n")
+        assert result.exit_code == 0
+        time.sleep(1)
         subprocess.run(
             "./redis-stack-server-7.2.0-v10/bin/redis-cli shutdown",
             shell=True,
