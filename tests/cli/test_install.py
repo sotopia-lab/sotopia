6bf0cd571f171d27a3b90e8f5612f30d944784fe
Hao <prokilchu@gmail.com>
2024-06-24 13:48:12 -0400

    give up testing docker on mac

--- tests/cli/test_install.py
+++ tests/cli/test_install.py
@@ -41,16 +41,16 @@ def test_install() -> None:
         )
 
     if platform.system() == "Darwin":
-        result = runner.invoke(app, ["install"], input="Yes\nNo\n\n")
-        assert result.exit_code == 0
-        time.sleep(1)
-        subprocess.run("docker stop redis-stack", shell=True, check=True)
-    elif platform.system() == "Linux":
         result = runner.invoke(app, ["install"], input="No\nNo\n")
         assert result.exit_code == 0
         time.sleep(1)
+        subprocess.run("redis-cli shutdown", shell=True, check=True)
+    elif platform.system() == "Linux":
+        result = runner.invoke(app, ["install"], input="Yes\nNo\n\n")
+        assert result.exit_code == 0
+        time.sleep(1)
         subprocess.run(
-            "./redis-stack-server-7.2.0-v10/bin/redis-cli shutdown",
+            "docker stop redis-stack",
             shell=True,
             check=True,
         )
