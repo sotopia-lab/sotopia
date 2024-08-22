f308a26068794a312c442e073cdace8320cd86b3
Hao <prokilchu@gmail.com>
2024-08-21 22:08:07 -0400

    Version conditioned import for `Self`

--- sotopia/database/persistent_profile.py
+++ sotopia/database/persistent_profile.py
@@ -1,5 +1,10 @@
 from enum import IntEnum
-from typing import Self
+import sys
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    from typing_extensions import Self
 
 from pydantic import model_validator
 from redis_om import JsonModel
