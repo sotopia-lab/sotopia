a06c201bb727cae1649a94dd5047e15f0b83d2cd
bekaboo <kankefengjing@gmail.com>
2025-03-19 16:11:05 -0400

    fix: `FileHandler` error if parent dir of log file does not exist

---
+++ sotopia/logging.py
@@ -0,0 +1,18 @@
+from logging import FileHandler as loggingFileHandler
+from pathlib import Path
+
+
+class FileHandler(loggingFileHandler):
+    """
+    A handler class which writes formatted logging records to disk files.
+
+    Wrapper of `logging.FileHandler` that creates parent directory of the log
+    file automatically.
+    """
+
+    def __init__(self, filename: str | Path, *args, **kwargs):
+        # ensure existence of parent directory
+        filename = Path(filename)
+        filename.parent.mkdir(parents=True, exist_ok=True)
+
+        super().__init__(filename, *args, **kwargs)
