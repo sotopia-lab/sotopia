4fcb00874f169045cbefd3038a6f41852370dfee
Keyu(Frank) He <frankhe@usc.edu>
2025-07-07 02:33:10 +0800

    Add scenarios from NegotiationArena

---
+++ examples/experimental/negotiation_arena/constants.py
@@ -0,0 +1,23 @@
+MONEY_TOKEN = "ZUP"
+
+RESOURCES_TAG = "my resources"
+GOALS_TAG = "my goals"
+REASONING_TAG = "reason"
+PLAYER_ANSWER_TAG = "player answer"
+PROPOSED_TRADE_TAG = "newly proposed trade"
+SPLIT_TAG = "split"
+MESSAGE_TAG = "message"
+VALUATION_TAG = "my valuation"
+REFUSING_OR_WAIT_TAG = "NONE"
+ACCEPTING_TAG = "ACCEPT"
+REJECTION_TAG = "REJECT"
+TURN_OR_MOVE_TAG = "move"
+PROPOSAL_COUNT_TAG = "proposal count"
+MY_NAME_TAG = "my name"
+
+OTHER_PLAYER_PROPOSED_TRADE = "other player proposed trade"
+OTHER_PLAYER_ANSWER = "other player answer"
+OTHER_PLAYER_MESSAGE = "other player message"
+
+AGENT_ONE = "Player RED"
+AGENT_TWO = "Player BLUE"
